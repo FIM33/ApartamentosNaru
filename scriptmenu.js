6f806ceb9790d88fcce0b0ea93808abdb054a536
FIM33 <95959404+FIM33@users.noreply.github.com>
2024-09-08 05:50:36 +0200

    Update scriptmenu.js

--- scriptmenu.js
+++ scriptmenu.js
@@ -1,4 +1,6 @@
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     const menu = document.querySelector('.nav-menu');
+    const icon = this.querySelector('i');
     menu.classList.toggle('show');
+    icon.classList.toggle('active'); // Cambia esta línea
 });
