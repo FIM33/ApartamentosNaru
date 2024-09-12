ca4c56d652adab284be0f46474c6f930edf542f5
FIM33 <95959404+FIM33@users.noreply.github.com>
2024-09-13 01:38:18 +0200

    Update scriptmenu.js

--- scriptmenu.js
+++ scriptmenu.js
@@ -1,4 +1,13 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    var navMenu = document.getElementById('nav-menu');
-    navMenu.classList.toggle('show');
+document.addEventListener('DOMContentLoaded', function() {
+    var menuToggle = document.querySelector('.menu-toggle');
+    var navMenu = document.querySelector('.nav-menu');
+
+    menuToggle.addEventListener('click', function() {
+        // Alterna la clase que controla la visibilidad del menú
+        if (navMenu.classList.contains('show')) {
+            navMenu.classList.remove('show');
+        } else {
+            navMenu.classList.add('show');
+        }
+    });
 });
