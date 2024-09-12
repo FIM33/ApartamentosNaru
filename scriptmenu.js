038e1241c1deb47119df71e17fb4a9e7f6e9031e
FIM33 <95959404+FIM33@users.noreply.github.com>
2024-09-12 21:43:31 +0200

    Update scriptmenu.js

--- scriptmenu.js
+++ scriptmenu.js
@@ -1,10 +1,10 @@
-// Seleccionar el botón del menú y el menú de navegación
-const menuToggle = document.querySelector('.menu-toggle');
-const navMenu = document.querySelector('.nav-menu');
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu');
 
-// Añadir un evento de clic al botón del menú
-menuToggle.addEventListener('click', () => {
-    // Alternar la clase 'active' en el menú de navegación y el botón del menú
-    navMenu.classList.toggle('active');
-    menuToggle.classList.toggle('active');
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('open');
+        menuToggle.classList.toggle('active');
+    });
 });
+
