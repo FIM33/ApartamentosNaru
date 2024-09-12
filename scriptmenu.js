4d92b9cb0387a2f50de823211657b9c575b1d799
FIM33 <95959404+FIM33@users.noreply.github.com>
2024-09-12 21:53:40 +0200

    Update scriptmenu.js

--- scriptmenu.js
+++ scriptmenu.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const menuToggle = document.querySelector('.menu-toggle');
     const navMenu = document.querySelector('.nav-menu');
-    
+
     menuToggle.addEventListener('click', function() {
-        // Toggle la clase 'open' para mostrar/ocultar el menú
+        // Alterna la clase 'open' para mostrar/ocultar el menú
         navMenu.classList.toggle('open');
-        // Toggle la clase 'active' para cambiar el color del icono de menú
+        // Alterna la clase 'active' para cambiar el color del icono de menú
         menuToggle.classList.toggle('active');
-        
-        // Gestionar el efecto cascada
+
+        // Gestiona el efecto de cascada
         if (navMenu.classList.contains('open')) {
-            // Si el menú está abierto, establecer la altura a auto para que se expanda
+            // Establece la altura del menú al valor de scrollHeight para mostrarlo
             navMenu.style.height = navMenu.scrollHeight + 'px';
         } else {
-            // Si el menú está cerrado, establecer la altura a 0 para ocultarlo
+            // Establece la altura del menú a 0 para ocultarlo
             navMenu.style.height = '0px';
         }
     });
